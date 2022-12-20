db7cf6ba821901f2c9b3acc851dc455e1a21f12e
eptrvsky <eptrvsky@gmail.com>
2022-12-20 08:36:52 +0200

    Update config.py

--- heroku-test/config.py
+++ heroku-test/config.py
@@ -16,4 +16,4 @@ WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
 # webserver settings
 WEBAPP_HOST = '0.0.0.0'
 WEBAPP_PORT = os.getenv('PORT', default=8000)
-DB_URL = os.getenv('DATABASE_URL')
+#DB_URL = os.getenv('DATABASE_URL')
